160c95160c737be3bd97f5e3357ceb3b4d845d95
Mateusz <86568486+Peol13@users.noreply.github.com>
2022-06-15 14:12:10 +0200

    simple style change

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./style.module.css";
 
 function Footer() {
-  return <footer className={styles.footer}>stworzone przez Peol13</footer>;
+  return <footer className={styles.footer}>Stworzone przez Peol13</footer>;
 }
 
 export default Footer;
